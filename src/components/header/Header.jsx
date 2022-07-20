15dfc6e5f657af4def6c73b1d64e4f5eea82deef
Nick <n.carlson.design@gmail.com>
2022-07-20 15:26:44 -0700

    Header mobile refactor

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -4,23 +4,27 @@ import styles from "./Header.module.scss";
 const Header = () => {
   return (
     <div className={styles.header}>
-      <div className={styles.column}>
-        <div className={styles.textblock}>
-          <span className={styles.heading}>Nicholas Carlson</span>
-          <p className={styles.paragraph}>
-            Talented UI/UX Designer with over 6 years of design and development
-            experience skilled in asking the important questions to enhance user
-            engagement and conversion.
-          </p>
-          <p className="subheading">
-            Seeking remote opportunities or on site assignments
-            <br />
-            in the grater Sacramento area.
-          </p>
-        </div>
-        <div className={styles.scroll}>
-          <p className="subheading">Scroll To See Work</p>
-          <img src="/images/scroll-arrow.png" alt="scroll arrow" />
+      <div className="container">
+        <div className="row justify-content-end">
+          <div className="col-md-7 align-self-end">
+            <div className={styles.textblock}>
+              <span className={styles.heading}>Nicholas Carlson</span>
+              <p className={styles.paragraph}>
+                Talented UI/UX Designer with over 6 years of design and
+                development experience skilled in asking the important questions
+                to enhance user engagement and conversion.
+              </p>
+              <p className={styles.tagline}>
+                Seeking remote opportunities or on site assignments in the
+                grater Sacramento area.
+              </p>
+            </div>
+          </div>
+
+          <div className={styles.scroll}>
+            <p>Scroll To See Work</p>
+            <img src="/images/scroll-arrow.png" alt="scroll arrow" />
+          </div>
         </div>
       </div>
     </div>
